5e37bb60e8928f0d7bbb21fe5a429f4508a6923e
chris <christopher.a.clarizio@gmail.com>
2017-11-25 13:52:29 -0500

    changed get, set, delete, methods from movies -> planets. commented out users, ratings methods

--- API/_planets_database.py
+++ API/_planets_database.py
@@ -1,48 +1,87 @@
 class _planets_database:
 
     def __init__(self):
-        self.planets     = {}
-        self.users      = {}
-        self.ratings    = {}
+        self.planets = {}		#{'planet1':{'Mass':1.2,'Type':1,...},'planet2':{'Mass':1.3,'Type':0,...},...}
     
-    def load_planets(self, movie_file, img_file):
-        self.movies     = {}
-        mvs = open(movie_file)
-        for line in mvs:
-            mov_arr = line.rstrip().split('::')
-            self.movies[int(mov_arr[0])] = {}
-            self.movies[int(mov_arr[0])]["id"] = int(mov_arr[0])          
-            self.movies[int(mov_arr[0])]["title"] = mov_arr[1]
-            self.movies[int(mov_arr[0])]["genre"] = mov_arr[2]
-            self.movies[int(mov_arr[0])]["img"] = None
-        mvs.close()
-        # load in images to each movie
-        imgs = open(img_file)
-        for line in imgs:
-            img_arr = line.rstrip().split('::')
-            self.movies[int(img_arr[0])]['img'] = img_arr[2]
-        imgs.close()
+    # Load information about all planets into database
+    def load_planets(self, planet_file):
+        file = open(planet_file)
+        for line in file:
+            planet_array = line.rstrip().split(',')
+            self.planets[str(planet_array[0])] = {}
+            self.planets[str(planet_array[0])]['type'] = int(planet_array[1])
+            self.planets[str(planet_array[0])]['mass'] = int(planet_array[2])
+            self.planets[str(planet_array[0])]['radius'] = int(planet_array[3])
+            self.planets[str(planet_array[0])]['period'] = int(planet_array[4])
+            self.planets[str(planet_array[0])]['semiMajorAxis'] = int(planet_array[5])
+            self.planets[str(planet_array[0])]['eccentricity'] = int(planet_array[6])
+            self.planets[str(planet_array[0])]['periastron'] = int(planet_array[7])
+            self.planets[str(planet_array[0])]['longitude'] = int(planet_array[8])
+            self.planets[str(planet_array[0])]['ascendingNode'] = int(planet_array[9])
+            self.planets[str(planet_array[0])]['inclination'] = int(planet_array[10])
+            self.planets[str(planet_array[0])]['surfaceTemperature'] = int(planet_array[11])
+            self.planets[str(planet_array[0])]['age'] = int(planet_array[12])
+            self.planets[str(planet_array[0])]['discoveryMethod'] = str(planet_array[13])
+            self.planets[str(planet_array[0])]['dicoveryYear'] = int(planet_array[14])
+            self.planets[str(planet_array[0])]['lastUpdated'] = str(planet_array[15])
+            self.planets[str(planet_array[0])]['rightAscension'] = str(planet_array[16])
+            self.planets[str(planet_array[0])]['declination'] = str(planet_array[17])
+            self.planets[str(planet_array[0])]['distanceFromSun'] = int(planet_array[18])
+            self.planets[str(planet_array[0])]['hostStarMass'] = int(planet_array[19])
+            self.planets[str(planet_array[0])]['hostStarRadius'] = int(planet_array[20])
+            self.planets[str(planet_array[0])]['hostStarMetallicity'] = int(planet_array[21])
+            self.planets[str(planet_array[0])]['hostStarTemperature'] = int(planet_array[22])
+            self.planets[str(planet_array[0])]['hostStarAge'] = int(planet_array[23])
+            self.planets[str(planet_array[0])]['listsPlanetIsOn'] = str(planet_array[24])
+        file.close()
 
-    def get_planet(self, mid):
-        if mid in self.movies:
-            return [self.movies[mid]["title"], self.movies[mid]["genre"]]
+    # Get information about single planet
+    def get_planet(self, pid):
+        if pid in self.planets:
+            return [self.planets[pid]['mass'], self.planets[pid]['radius'], self.planets[pid]['period']]
         return None
 
-    def get_planet(self):
-        movies = dict()
-        for key in self.movies:
-            movies[key] = self.movies[key]
-        return movies
+    # Get infomation about all planets in database
+    def get_planets(self):
+        planets = dict()
+        for key in self.planets:
+            planets[key] = self.planets[key]
+        return planets
 
-    def set_planet(self, mid, inp):
-        if mid not in self.movies:
-            self.movies[mid] = {}
-        self.movies[mid]["title"] = inp[0]
-        self.movies[mid]["genre"] = inp[1]
+    # Set information about a single planet, adds new entry if planet not present in database
+    def set_planet(self, pid, inp):
+        if pid not in self.planets:
+            self.planets[pid] = {}
+        self.planets[pid]['type'] = int(inp[1])
+        self.planets[pid]['mass'] = int(inp[2])
+        self.planets[pid]['radius'] = int(inp[3])
+        self.planets[pid]['period'] = int(inp[4])
+        self.planets[pid]['semiMajorAxis'] = int(inp[5])
+        self.planets[pid]['eccentricity'] = int(inp[6])
+        self.planets[pid]['periastron'] = int(inp[7])
+        self.planets[pid]['longitude'] = int(inp[8])
+        self.planets[pid]['ascendingNode'] = int(inp[9])
+        self.planets[pid]['inclination'] = int(inp[10])
+        self.planets[pid]['surfaceTemperature'] = int(inp[11])
+        self.planets[pid]['age'] = int(inp[12])
+        self.planets[pid]['discoveryMethod'] = str(inp[13])
+        self.planets[pid]['dicoveryYear'] = int(inp[14])
+        self.planets[pid]['lastUpdated'] = str(inp[15])
+        self.planets[pid]['rightAscension'] = str(inp[16])
+        self.planets[pid]['declination'] = str(inp[17])
+        self.planets[pid]['distanceFromSun'] = int(inp[18])
+        self.planets[pid]['hostStarMass'] = int(inp[19])
+        self.planets[pid]['hostStarRadius'] = int(inp[20])
+        self.planets[pid]['hostStarMetallicity'] = int(inp[21])
+        self.planets[pid]['hostStarTemperature'] = int(inp[22])
+        self.planets[pid]['hostStarAge'] = int(inp[23])
+        self.planets[pid]['listsPlanetIsOn'] = str(inp[24])
 
-    def delete_planet(self, mid):
-        self.movies.pop(mid, None)
+    # Deletes entry for specified planet in database
+    def delete_planet(self, pid):
+        self.planets.pop(pid, None)
 
+'''
     def load_users(self, user_file):
         self.users     = {}
         mvs = open(user_file)
@@ -131,3 +170,4 @@ class _planets_database:
     def print_sorted_movies(self):
         for mov in sorted(self.movies):
             print(mov)
+'''
